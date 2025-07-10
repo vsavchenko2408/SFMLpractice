a01d70f8ddba2e1d1eb09f13bac1521d8dac3bbd
DerMeister <v.savchenko2408@gmail.com>
2025-07-10 22:26:26 +0300

    create crosslight

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Main window");
+    sf::RenderWindow window(sf::VideoMode({200, 600}), "Main window");
+    sf::CircleShape red(90, 90);
+    red.move(sf::Vector2f(10, 10));
+    red.setFillColor(sf::Color::Red);
+    sf::CircleShape yellow(90, 90);
+    yellow.move(sf::Vector2f(10, 210));
+    yellow.setFillColor(sf::Color::Yellow);
+    sf::CircleShape green(90, 90);
+    green.move(sf::Vector2f(10, 410));
+    green.setFillColor(sf::Color::Green);
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -11,8 +20,17 @@ int main()
                 window.close();
         }
 
-        window.clear();
-
+        window.clear(sf::Color::Black);
+        window.draw(red);
         window.display();
+        sf::sleep(std::chrono::milliseconds(1000));
+        window.clear(sf::Color::Black);
+        window.draw(yellow);
+        window.display();
+        sf::sleep(std::chrono::milliseconds(1000));
+        window.clear(sf::Color::Black);
+        window.draw(green);
+        window.display();
+        sf::sleep(std::chrono::milliseconds(1000));
     }
 }
