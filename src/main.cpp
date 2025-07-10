78caa32c0a6ddc24743c3337dd4aa3329ed0b4d4
DerMeister <v.savchenko2408@gmail.com>
2025-07-10 20:40:10 +0300

    created while

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({320, 240}), "Main window");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Main window");
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
 }
