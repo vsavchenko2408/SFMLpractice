8f70e4ba1b565636033914708f2c57b281b3e1dd
DerMeister <v.savchenko2408@gmail.com>
2025-07-10 20:21:19 +0300

    add SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({320, 240}), "SFML works!");
+    while (window.isOpen())
+    {
+        // window.clear();
+        window.display();
+    }
+}
