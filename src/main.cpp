1ac79f299a4439bc500276ee3ca81b5b142e4662
DerMeister <v.savchenko2408@gmail.com>
2025-07-13 10:47:14 +0300

    roof

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,10 @@ int main()
     sf::ConvexShape roof;
     roof.setFillColor(sf::Color(120, 50, 50));
     roof.setPointCount(4);
-    roof.setPoint(0, {250, 150});
-    roof.setPoint(1, {550, 150});
-    roof.setPoint(2, {650, 450});
-    roof.setPoint(3, {150, 450});
+    roof.setPoint(0, {150, 100});
+    roof.setPoint(1, {650, 100});
+    roof.setPoint(2, {750, 250});
+    roof.setPoint(3, {50, 250});
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
